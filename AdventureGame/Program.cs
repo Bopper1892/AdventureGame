c5dc30a2770125feaf06633f4115a00b0a5867d4
MattJ <matt_joinson@hotmail.co.uk>
2018-07-10 11:47:46 +0100

    Add project files.

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            string characterName = "Wizard";
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+            Console.WriteLine("Flynn's Amazing Adventure Game!");
+            Console.WriteLine("Welcome to my magical world of fun!\n");
+            Console.WriteLine("OK, let's get started by setting up your character.");
+            //Capture user name choice
+            Console.Write("Enter a name for your character: ");
+            characterName = Console.ReadLine();
+            Console.WriteLine("{1}", characterName, myTI.ToTitleCase(characterName) + "! What a cool name!\n");
+            Console.ReadLine();
+        }
+
+
+    }
+}
